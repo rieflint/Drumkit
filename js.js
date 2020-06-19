e3b8744b7b270aacbcf36b1ebac9c3ff31f806ed
rieflint <57578036+rieflint@users.noreply.github.com>
2020-06-19 19:07:35 -0400

    Updated Footer

--- js.js
+++ js.js
@@ -1,43 +1,43 @@
-var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+var numberOfDrumButtons = document.querySelectorAll(".drum").length
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    var buttonInnerHTML = this.innerHTML;
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML
     switch (buttonInnerHTML) {
       case "W":
-        var crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
+        var crash = new Audio("sounds/crash.mp3")
+        crash.play()
+        break
       case "A":
-        var kick = new Audio("sounds/kick-bass.mp3");
-        kick.play();
-        break;
+        var kick = new Audio("sounds/kick-bass.mp3")
+        kick.play()
+        break
       case "S":
-        var snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
+        var snare = new Audio("sounds/snare.mp3")
+        snare.play()
+        break
       case "D":
-        var tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
+        var tom1 = new Audio("sounds/tom-1.mp3")
+        tom1.play()
+        break
       case "J":
-        var tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
+        var tom2 = new Audio("sounds/tom-2.mp3")
+        tom2.play()
+        break
       case "K":
-        var tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
+        var tom3 = new Audio("sounds/tom-3.mp3")
+        tom3.play()
+        break
       case "L":
-        var tom4 = new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break;
+        var tom4 = new Audio("sounds/tom-4.mp3")
+        tom4.play()
+        break
 
       default:
-        console.log(buttonInnerHTML);
-        break;
+        console.log(buttonInnerHTML)
+        break
     }
-  });
+  })
 }
 
 // var audio = new Audio("sounds/tom-1.mp3");
