3c9be597fb96a448002048c838018160e27f9b2d
rieflint <57578036+rieflint@users.noreply.github.com>
2020-06-20 16:36:35 -0400

    Updated animation

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//Detecting Button Press
-
 var numberOfDrumButtons = document.querySelectorAll(".drum").length
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
@@ -12,8 +10,6 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
   })
 }
 
-//Detecting Keyboard Press -- keydown
-
 document.addEventListener("keydown", function (event) {
   makeSound(event.key)
 
@@ -23,47 +19,51 @@ document.addEventListener("keydown", function (event) {
 function makeSound(key) {
   switch (key) {
     case "w":
-      var crash = new Audio("sounds/crash.mp3")
-      crash.play()
-      break
-    case "a":
-      var kick = new Audio("sounds/kick-bass.mp3")
-      kick.play()
-      break
-    case "s":
-      var snare = new Audio("sounds/snare.mp3")
-      snare.play()
-      break
-    case "d":
       var tom1 = new Audio("sounds/tom-1.mp3")
       tom1.play()
       break
-    case "j":
+
+    case "a":
       var tom2 = new Audio("sounds/tom-2.mp3")
       tom2.play()
       break
-    case "k":
+
+    case "s":
       var tom3 = new Audio("sounds/tom-3.mp3")
       tom3.play()
       break
-    case "lw":
+
+    case "d":
       var tom4 = new Audio("sounds/tom-4.mp3")
       tom4.play()
       break
 
+    case "j":
+      var snare = new Audio("sounds/snare.mp3")
+      snare.play()
+      break
+
+    case "k":
+      var crash = new Audio("sounds/crash.mp3")
+      crash.play()
+      break
+
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3")
+      kick.play()
+      break
+
     default:
       console.log(key)
-      break
   }
 }
 
 function buttonAnimation(currentKey) {
   var activeButton = document.querySelector("." + currentKey)
+
   activeButton.classList.add("pressed")
 
   setTimeout(function () {
     activeButton.classList.remove("pressed")
   }, 100)
 }
-// var audio = new Audio("sounds/tom-1.mp3");
-// audio.play();
